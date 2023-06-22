8f92a1f7e10c424e49106b6fb0f6a1fc2804781a
ChiefLopes <chieflopesekede@gmail.com>
2023-06-22 12:44:59 +0100

    playing around with Array map function

---
+++ The map Array-Method/main.js
@@ -0,0 +1,15 @@
+let people = [
+    { name: "John", age: 41, job: "Developer" },
+    { name: "Jacqlin", age: 31, job: "Teacher" },
+    { name: "Sunshine", age: 20, job: "Developer" },
+    { name: "Sisely", age: 28, job: "coach" },
+    { name: "John", age: 36, job: "Nurse" },
+    { name: "John", age: 59, job: "Doctor" },
+    { name: "Hillard", age: 17, job: "Police" },
+    { name: "Randee", age: 46, job: "Firefighter" },
+    { name: "John", age: 19, job: "Author" },
+    { name: "Robert", age: 58, job: "Developer" },
+];
+
+const peoplename = people.map(name => name.name)
+console.log(peoplename);
